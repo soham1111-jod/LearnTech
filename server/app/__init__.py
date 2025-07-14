a2ad002065c9c0b0fc37660170061df9efdbb80a
Soham Chafale <sohamchafale11@gmail.com>
2025-07-14 12:48:43 +0530

    update app

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -7,12 +7,14 @@ from app.routes.playlist_routes import playlist_bp
 from app.routes.admin_project_routes import admin_project_bp
 from app.errors.handlers import register_error_handlers
 import os
+from flask import Blueprint, jsonify
+from app.commands.ingest_and_summarize import run_ingest_and_summarize
+from flask import request
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object('config.Config')
-    CORS(app, origins=["http://localhost:5173",
-        "http://127.0.0.1:5173"], supports_credentials=True)
+    CORS(app, origins=["https://learn-tech-brown.vercel.app/"], supports_credentials=True)
 
     # Initialize MongoDB
     init_mongo(app)
@@ -29,6 +31,15 @@ def create_app():
     # Register CLI commands
     from app.commands.ingest_and_summarize import ingest_and_summarize_command
     app.cli.add_command(ingest_and_summarize_command)
+
+    @app.route('/api/cron/ingest', methods=['POST'])
+    def trigger_ingest():
+        # Optional: Add a simple auth check here for security
+        try:
+            run_ingest_and_summarize()
+            return jsonify({"status": "success"}), 200
+        except Exception as e:
+            return jsonify({"status": "error", "message": str(e)}), 500
 
     @app.route('/')
     def home():
